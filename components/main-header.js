6627d497bfe0fae5a6c90f17a2134827c55e4a13
Leslie Chou <chouyuchien@gmail.com>
2024-01-15 21:51:11 -0800

    finished styling for main-header component

--- components/main-header.js
+++ components/main-header.js
@@ -1,27 +1,30 @@
 import Link from "next/link";
 
 import logoImg from '@/assets/logo.png';
-// console.log("logoImg", logoImg);
+
+import classes from './main-header.module.css'
 
 export default function MainHeader() {
 
   return (
     <>
-    <header>
-        <Link href='/'>
+    <header className={classes.header}>
+        <Link href='/' className={classes.logo}>
             <img src={logoImg.src} alt="NextLevel Food" />
             NextLevel Food
         </Link>
+    
+        <nav className={classes.nav}>
+            <ul >
+                <li>
+                    <Link href='/meals'>Browse Meals</Link>
+                </li>
+                <li>
+                    <Link href='/community'>Foodies Community</Link>
+                </li>
+            </ul>
+        </nav>
     </header>
-
-    <ul>
-        <li>
-            <Link href='/meals'>Browse Meals</Link>
-        </li>
-        <li>
-            <Link href='/community'>Foodies Community</Link>
-        </li>
-    </ul>
     </>
   )
 }
