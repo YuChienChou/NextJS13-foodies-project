8c84f2dead4259f35e773a1c98396bde561a7e64
Leslie Chou <chouyuchien@gmail.com>
2024-01-16 21:54:04 -0800

    added image-slideshow and nav-link components and finished styling

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,33 +1,34 @@
+
 import Link from "next/link";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "../nav-link";
 
 import logoImg from '@/assets/logo.png';
-// console.log("logoImg", logoImg);
 import classes from './main-header.module.css'
 
 export default function MainHeader() {
 
-  return (
-    <>
-    <MainHeaderBackground />
-    <header className={classes.header}>
-        <Link href='/' className={classes.logo}>
-            <Image src={logoImg} alt="NextLevel Food" priority />
-            NextLevel Food
-        </Link>
-    
-        <nav className={classes.nav}>
-            <ul >
-                <li>
-                    <Link href='/meals'>Browse Meals</Link>
-                </li>
-                <li>
-                    <Link href='/community'>Foodies Community</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    </>
-  )
+    return (
+        <>
+        <MainHeaderBackground />
+        <header className={classes.header}>
+            <Link href='/' className={classes.logo}>
+                <Image src={logoImg} alt="NextLevel Food" priority />
+                NextLevel Food
+            </Link>
+        
+            <nav className={classes.nav}>
+                <ul >
+                    <li>
+                        <NavLink href='/meals'>Browse Meals</NavLink>
+                    </li>
+                    <li>
+                        <NavLink href='/community'>Foodies Community</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
 }
