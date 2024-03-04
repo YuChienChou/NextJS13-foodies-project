ae281d33808583ea4e972f15249d62a989c0517d
Leslie Chou <chouyuchien@gmail.com>
2024-03-03 16:00:11 -0800

    added error handling and 404 page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>Page not found!</h1>
+            <p>Could not find the requested page or resource.</p>
+        </main>
+    )
+}
