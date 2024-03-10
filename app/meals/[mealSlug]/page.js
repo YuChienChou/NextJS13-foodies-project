8f0a3c6ef8a5eeeb032348084bf4b6c12b1db785
Leslie Chou <chouyuchien@gmail.com>
2024-03-09 17:11:21 -0800

    finished meal details page and finished styling

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,39 @@
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
 
-export default function MealDetailsPage() {
+
+export default function MealDetailsPage({params}) {
+
+  // console.log("params: ", params);
+
+  const meal = getMeal(params.mealSlug);
+  // console.log("meal: ", meal);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br>');  // replace newlines with <br> tags
+
   return (
-    <main>
-      <h1>Meals Detail</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>
+            {meal.summary}
+          </p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions,
+        }}>
+        </p>
+      </main>
+    </>
   )
 }
