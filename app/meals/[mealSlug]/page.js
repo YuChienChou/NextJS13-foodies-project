94d2ad6dc49551b0c7bc5501d2ce91a52cdebe19
Leslie Chou <chouyuchien@gmail.com>
2024-03-11 20:53:03 -0700

    added not-found page in the meal folder for missing meal data

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
 
 
 export default function MealDetailsPage({params}) {
@@ -9,6 +10,10 @@ export default function MealDetailsPage({params}) {
 
   const meal = getMeal(params.mealSlug);
   // console.log("meal: ", meal);
+
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, '<br>');  // replace newlines with <br> tags
 
