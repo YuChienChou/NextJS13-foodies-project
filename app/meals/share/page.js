c0a5e45e2628c54d47d41c01160b505d7bff904c
Leslie Chou <chouyuchien@gmail.com>
2024-03-15 16:42:58 -0700

    implemented server action for share meal form submission

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,9 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
+  
   return (
     <>
       <header className={classes.header}>
@@ -11,11 +13,11 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}> {/* instead of using OnClick, using action and assign the server function to be triggered when the form is submitted */}
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <input type="text" id="name" name="name" required /> 
             </p>
             <p>
               <label htmlFor="email">Your email</label>
@@ -39,8 +41,8 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label="Image" name="image" />
           <p className={classes.actions}>
+          <ImagePicker label="Your image" name="image" />
             <button type="submit">Share Meal</button>
           </p>
         </form>
