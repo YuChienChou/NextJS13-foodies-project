e764f0393f079039bd27e663162ce6dd89df2646
Leslie Chou <chouyuchien@gmail.com>
2024-03-23 17:25:37 -0700

    added Next.js cache revalidations function for production environment when new meal is added.

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,17 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+
 import ImagePicker from '@/components/meals/image-picker';
-import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
+import classes from './page.module.css';
+
+
 export default function ShareMealPage() {
+
+  const [state, formAction] = useFormState(shareMeal, {message: null});
   
   return (
     <>
@@ -14,7 +22,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}> {/* instead of using OnClick, using action and assign the server function to be triggered when the form is submitted */}
+        <form className={classes.form} action={formAction}> {/* instead of using OnClick, using action and assign the server function to be triggered when the form is submitted */}
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,9 +51,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <p className={classes.actions}>
-          <ImagePicker label="Your image" name="image" />
-            {/* <button type="submit">Share Meal</button> */}
-          <MealsFormSubmit />
+            <ImagePicker label="Your image" name="image" />
+            {state.message && <p>{state.message}</p>}
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
